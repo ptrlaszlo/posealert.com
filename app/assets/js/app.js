b0f410fbdf1697698333289d61480b1a607eccbb
Peter Laszlo <peter.laszlo@tnt-digital.com>
2019-09-24 21:32:14 +0200

    Remove inner functions

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -108,77 +108,77 @@ function detectPoseInRealTime(video, net) {
   const canvas = document.getElementById('output');
   const ctx = canvas.getContext('2d');
 
+  canvas.width = videoWidth;
+  canvas.height = videoHeight;
+
+  poseDetectionFrame(video, net, ctx);
+}
+
+async function poseDetectionFrame(video, net, ctx) {
+  let poses = [];
+  let minPoseConfidence;
+  let minPartConfidence;
+
   // since images are being fed from a webcam, we want to feed in the
   // original image and then just flip the keypoints' x coordinates. If instead
   // we flip the image, then correcting left-right keypoint pairs requires a
   // permutation on all the keypoints.
   const flipPoseHorizontal = true;
 
-  canvas.width = videoWidth;
-  canvas.height = videoHeight;
+  const pose = await net.estimatePoses(video, {
+    flipHorizontal: flipPoseHorizontal,
+    decodingMethod: 'single-person'
+  });
 
-  async function poseDetectionFrame() {
-    let poses = [];
-    let minPoseConfidence;
-    let minPartConfidence;
-
-    const pose = await net.estimatePoses(video, {
-      flipHorizontal: flipPoseHorizontal,
-      decodingMethod: 'single-person'
-    });
-
-    if ( Math.floor((Math.random() * 100) + 1) == 60 ) {
-      console.log(pose);
-    }
-    poses = poses.concat(pose);
-    minPoseConfidence = 0.4;//+guiState.singlePoseDetection.minPoseConfidence;
-    minPartConfidence = 0.6;//+guiState.singlePoseDetection.minPartConfidence;
-
-    ctx.clearRect(0, 0, videoWidth, videoHeight);
-
-    // if (guiState.output.showVideo) {
-    ctx.save();
-    ctx.scale(-1, 1);
-    ctx.translate(-videoWidth, 0);
-    ctx.drawImage(video, 0, 0, videoWidth, videoHeight);
-    ctx.restore();
-    // }
-
-    function drawKeypoints(keypoints, minConfidence, ctx, scale = 1) {
-      for (let i = 0; i < keypoints.length; i++) {
-        const keypoint = keypoints[i];
-
-        if (keypoint.score < minConfidence) {
-          continue;
-        }
-
-        const {y, x} = keypoint.position;
-        const color = "#ff2626";
-        drawPoint(ctx, y * scale, x * scale, 3, color, keypoint.part + ' ' + keypoint.score.toFixed(4));
-      }
-    }
-
-    // For each pose (i.e. person) detected in an image, loop through the poses
-    // and draw the resulting skeleton and keypoints if over certain confidence
-    // scores
-    poses.forEach(({score, keypoints}) => {
-      if (score >= minPoseConfidence) {
-        // if (guiState.output.showPoints) {
-          drawKeypoints(keypoints, minPartConfidence, ctx);
-        // }
-        // if (guiState.output.showSkeleton) {
-        //   drawSkeleton(keypoints, minPartConfidence, ctx);
-        // }
-        // if (guiState.output.showBoundingBox) {
-        //   drawBoundingBox(keypoints, ctx);
-        // }
-      }
-    });
-
-    requestAnimationFrame(poseDetectionFrame);
+  if ( Math.floor((Math.random() * 100) + 1) == 60 ) {
+    console.log(pose);
   }
+  poses = poses.concat(pose);
+  minPoseConfidence = 0.4;
+  minPartConfidence = 0.6;
 
-  poseDetectionFrame();
+  ctx.clearRect(0, 0, videoWidth, videoHeight);
+
+  // if (guiState.output.showVideo) {
+  ctx.save();
+  ctx.scale(-1, 1);
+  ctx.translate(-videoWidth, 0);
+  ctx.drawImage(video, 0, 0, videoWidth, videoHeight);
+  ctx.restore();
+  // }
+
+  // For each pose (i.e. person) detected in an image, loop through the poses
+  // and draw the resulting skeleton and keypoints if over certain confidence
+  // scores
+  poses.forEach(({score, keypoints}) => {
+    if (score >= minPoseConfidence) {
+      // if (guiState.output.showPoints) {
+        drawKeypoints(keypoints, minPartConfidence, ctx);
+      // }
+      // if (guiState.output.showSkeleton) {
+      //   drawSkeleton(keypoints, minPartConfidence, ctx);
+      // }
+      // if (guiState.output.showBoundingBox) {
+      //   drawBoundingBox(keypoints, ctx);
+      // }
+    }
+  });
+
+  requestAnimationFrame(poseDetectionFrame(video, net, ctx));
+}
+
+function drawKeypoints(keypoints, minConfidence, ctx, scale = 1) {
+  for (let i = 0; i < keypoints.length; i++) {
+    const keypoint = keypoints[i];
+
+    if (keypoint.score < minConfidence) {
+      continue;
+    }
+
+    const {y, x} = keypoint.position;
+    const color = "#ff2626";
+    drawPoint(ctx, y * scale, x * scale, 3, color, keypoint.part + ' ' + keypoint.score.toFixed(4));
+  }
 }
 
 function drawPoint(ctx, y, x, r, color, text) {
